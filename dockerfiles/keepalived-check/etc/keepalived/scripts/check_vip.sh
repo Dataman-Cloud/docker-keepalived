21cf4f04f0ccb0a2e31016edc192267c3fd0acd0
ss75710541 <75710541@qq.com>
2017-06-07 15:20:02 +0800

    update keepalived-base ,fix etc/keepalived/scripts/check_vip.sh log error

--- dockerfiles/keepalived-check/etc/keepalived/scripts/check_vip.sh
+++ dockerfiles/keepalived-check/etc/keepalived/scripts/check_vip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 vip=`awk '/dev.*label*/{print $1}' /etc/keepalived/keepalived.conf|awk -F/ '{print $1}'`
 
-tail -1 /var/log/keepalived-status.log | grep master || exit 0
+tail -1 /var/log/keepalived-status.log 2>/dev/null | grep master &>/dev/null || exit 0
 
 ip a | grep $vip >/dev/null 2>&1
 if [  $? -ne 0 ];then
